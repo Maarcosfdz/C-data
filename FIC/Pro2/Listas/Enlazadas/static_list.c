af3c3ad7e9a0942d844c352d1761de486ad61c1d
Maarcosfdz <126235697+Maarcosfdz@users.noreply.github.com>
2023-05-29 20:54:12 +0200

    Update static_list.c

--- FIC/Pro2/Listas/Enlazadas/static_list.c
+++ FIC/Pro2/Listas/Enlazadas/static_list.c
@@ -23,53 +23,14 @@ tPosL previous(tPosL p, tList L) {
 }
 
 tPosL next(tPosL p, tList L) {
-    if (p == L.lastPos) {
+    if (p == L.LastPos) 
         return LNULL;
-    } else {
+    else 
         return ++p;
-    }
 }
 
-bool insertItem(tItemL d, tPosL p, tList *L) {
-    /* i -> Iterador que moverá todos los elementos una posición más adelante en caso de que se inserte el elemento
-     * por el medio de la lista, este comenzará desde el final e irá hasta la posición en la que lo querramos añadir */
-    tPosL i;
-    bool isInserted;
-
-    if(L->lastPos == MAX - 1) { // Comprueba si la lista está llena
-        isInserted = false;
-    } else {
-        L->lastPos++; // Como estamos insertando un elemento, al tamaño del array aumenta en 1
-
-        if(p == LNULL) { // Si se quiere insertar en la última posición
-            L->data[L->lastPos] = d;
-
-        } else { // Si se quiere insertar en primera posición o cualquier otra intermedia
-            // Desplazamos todos los elementos una posición más adelante en el array
-            for(i = L->lastPos; i > p; i--) {
-                L->data[i] = L->data[i - 1];
-            }
-
-            L->data[p] = d;
-        }
-
-        isInserted = true;
-    }
-    return isInserted;
-}
-
-bool copyList(tList L, tList *M) { // Copiar la lista de un array en otro
-    tPosL p;
-
-    createEmptyList(M);
-
-    for(p = 0; p <= L.lastPos; p++) { // Desde la primera posición de L hasta la última
-        M->data[p] = L.data[p]; // Iremos igualando el campo data de cada elemento
-    }
-
-    M->lastPos = L.lastPos; // Como son dos listas idénticas, la última posición será la misma en ambas
-
-    return true;
+tItemL getItem(tPosL p, tList L) {
+    return L.data[p];
 }
 
 void updateItem(tItemL d, tPosL p, tList *L) {
@@ -83,9 +44,8 @@ tPosL findItem(tItemL d, tList L) {
     if(isEmptyList(L)) {
         return LNULL;
     } else {
-        for(p = 0; (p < L.lastPos) && (L.data[p] != d); p++);
-
-        if(L.data[p] == d) {
+        for(p = 0; (p < L.LastPos) && (L.data[p] != d); p++); //bucle que recorre a lista
+        if(L.data[p] == d) { 
             return p;
         } else {
             return LNULL;
@@ -94,8 +54,47 @@ tPosL findItem(tItemL d, tList L) {
 
 }
 
-tItemL getItem(tPosL p, tList L) {
-    return L.data[p];
+bool insertItem(tItemL d, tPosL p, tList *L) {
+    /* i -> Iterador que moverá todos los elementos una posición más adelante en caso de que se inserte el elemento
+     * por el medio de la lista, este comenzará desde el final e irá hasta la posición en la que lo querramos añadir */
+    tPosL i;
+
+    if(L->LastPos == MAX - 1) { // Comprueba si la lista está llena, o de -1, é porque a última posición, ao ser un string está reservada para o \0
+        return false;
+    } else {
+        L->lastPos++; // Como estamos insertando un elemento, al tamaño del array aumenta en 1
+
+        if(p == LNULL) { // Si se quiere insertar en la última posición
+            L->data[L->LastPos] = d;
+
+        } else { // Si se quiere insertar en primera posición o cualquier otra intermedia
+            // Desplazamos todos los elementos una posición más adelante en el array
+            for(i = L->LastPos; i > p; i--) {
+                L->data[i] = L->data[i - 1];
+            }
+
+            L->data[p] = d;
+        }
+    return true;
+    }
+}
+
+void deleteList(tList *L) {
+    L->LastPos = LNULL;
+}
+
+bool copyList(tList L, tList *M) { // Copiar la lista de un array en otro
+    tPosL p;
+
+    createEmptyList(M);
+
+    for(p = 0; p <= L.LastPos; p++) { // Desde la primera posición de L hasta la última
+        M->data[p] = L.data[p]; // Iremos igualando el campo data de cada elemento
+    }
+
+    M->LastPos = L.LastPos; // Como son dos listas idénticas, la última posición será la misma en ambas
+
+    return true;
 }
 
 void deleteAtPosition(tPosL p, tList *L) {
@@ -109,8 +108,4 @@ void deleteAtPosition(tPosL p, tList *L) {
         L->data[i] = L->data[i + 1];
     }
 
-}
-
-void deleteList(tList *L) {
-    L->lastPos = LNULL;
 }
