3e739e8b57907714d88b2a37e815c86fa565ab74
Maarcosfdz <126235697+Maarcosfdz@users.noreply.github.com>
2023-06-01 14:03:57 +0200

    Create recorridosárboles.h

---
+++ FIC/Pro2/TAD/Árboles/Binarios/recorridosárboles.h
@@ -0,0 +1,19 @@
+#ifndef TAD_RECORRIDOSYFUNCIONESEXTRA_H
+#define TAD_RECORRIDOSYFUNCIONESEXTRA_H
+
+#include "dinamicosBinNormales.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+// RECORRIDOS
+void preorder(tBinTree T);
+void inorder(tBinTree T);
+void postorder(tBinTree T);
+
+// FUNCIONES EXTRA
+int countNodes(tBinTree T);                             // Cuentas cuantos nodos hay en el árbol
+int countL(tBinTree T);                                 // Cuenta cuantas hojas hay (nodo sin hijos)
+int height(tBinTree T);                                 // Cuenta la altura máxima del árbol
+bool isFullTree(tBinTree T);
+
+#endif //TAD_RECORRIDOSYFUNCIONESEXTRA_H
